41a35c1d08ddac5dda839befc457c346650692c3
Matias Emanuel Pace <matiasepace@hotmail.com>
2025-06-07 14:40:41 -0300

    remove Retail-Data references from Retail-Controller.ts

--- src/Retail/Retail-Controller.ts
+++ src/Retail/Retail-Controller.ts
@@ -1,6 +1,2 @@
 import { Request, Response, NextFunction } from "express";
-import retailData from "../Data/RetailData";
 
-function findAll(req: Request, res: Response) {
-  res.json(retailData);
-}
