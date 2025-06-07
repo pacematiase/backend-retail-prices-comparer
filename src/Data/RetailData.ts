21aa9e3b21d1c20ff1b3285d6da35f1a13f769b2
Matias Emanuel Pace <matiasepace@hotmail.com>
2025-06-07 14:38:14 -0300

    Delete src/Data/RetailData.ts since it was moved to src/Retail/Retail-Repository.ts

--- src/Data/RetailData.ts
+++
@@ -1,8 +0,0 @@
-import { Retail } from "../Retail/Retail-Entity";
-const retailData: Retail[] = [
-  new Retail(1, "coto"),
-  new Retail(2, "Carrefour"),
-  new Retail(3, "La gallega"),
-];
-
-export default retailData;
